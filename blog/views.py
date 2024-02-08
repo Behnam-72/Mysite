5c11c0fad9d234def80d5b0d350d538cf4cf029f
Behnam-72 <bagherib21@gmail.com>
2024-02-08 15:13:46 +0330

    next and prev
    next and prev

--- blog/views.py
+++ blog/views.py
@@ -8,8 +8,8 @@ def blog_page(request):
 
 def single_page(request, pid):
     post = get_object_or_404(Post, pk=pid, status=True, published_date__lte=timezone.now())
-    post_previous = Post.objects.filter(id__lt=pid, status=True, published_date__lte=timezone.now()).order_by('published_date').last()
-    post_next = Post.objects.filter(id__gt=pid, status=True, published_date__lte=timezone.now()).order_by('published_date').first()
+    post_previous = Post.objects.filter(id__lt=pid, status=True, published_date__lte=timezone.now()).last()
+    post_next = Post.objects.filter(id__gt=pid, status=True, published_date__lte=timezone.now()).first()
     post.counted_view += 1
     post.save()
     context = {'post': post, 'post_previous': post_previous, 'post_next':post_next}
