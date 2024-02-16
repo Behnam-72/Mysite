43be632ac8f38109c846c341f119f854c3bd9c98
Behnam-72 <bagherib21@gmail.com>
2024-02-16 11:52:39 +0330

    site-map
    sitemap done

--- website/models.py
+++ website/models.py
@@ -3,16 +3,18 @@ from django.db import models
 class Contact(models.Model):
     name = models.CharField(max_length=250)
     email = models.EmailField()
-    subject = models.CharField(max_length=250)
+    subject = models.CharField(max_length=250, blank=True, null=True)
     message = models.TextField()
     creaded_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
+    
     class Meta:
         ordering = ['creaded_date']
         
-    def __str__(self):
-        return self.subject
+    
     
 class Newsletter(models.Model):
     email = models.EmailField()
--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from website.forms import ContactForm, NewsletterForm
+from django.contrib import messages
+
 def home_page(request):
     return render(request, 'website/index.html')
 
@@ -13,7 +15,13 @@ def contact_page(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
+            new_name = form.save()
+            new_name.name = 'Unknown'
+            new_name.save()
             form.save()
+            messages.add_message(request, messages.SUCCESS, 'Your message submitted successfully')
+        else:
+            messages.add_message(request, messages.ERROR, 'Your message didnt submit')
     form = ContactForm() 
     return render(request, 'website/contact.html', {'form': form})
 
