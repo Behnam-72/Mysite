132f08f23957419cc362e62856fb9ac89a4324e9
Behnam-72 <140879294+Behnam-72@users.noreply.github.com>
2024-04-04 13:32:11 +0330

    login with email or username
    login with email or username and show message if enter wrong email, username or password

--- accounts/views.py
+++ accounts/views.py
@@ -2,25 +2,28 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
-from website.forms import RegisterForm
+from accounts.forms import RegisterForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+import re
 def login_view(request):
     if not request.user.is_authenticated:
         if request.method == 'POST':
-            form = AuthenticationForm(request=request, data=request.POST)
-            if form.is_valid():
-                username = form.cleaned_data.get('username')
-                email = form.cleaned_data.get('username')
-                password = form.cleaned_data.get('password')
-                try:
-                    user = authenticate(request, username=username, password=password)
-                    if user is not None:
-                        login(request, user)
-                        return redirect('/')
-                except:
-                    user = authenticate(request, email=email, password=password)
-                    if user is not None:
-                        login(request, user)
-                        return redirect('/')
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+            if re.match(r'^[\w\.-]+@[\w\.-]+\.\w+$', username):
+                email = username
+                user = User.objects.filter(email=email).first()
+                if user:
+                    username = user.username
+                user = authenticate(request, username=username, password=password)
+            else:
+                user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+            else:
+                messages.add_message(request, messages.INFO, "Login Failed! Please enter the username and password correctly.")
         
         form = AuthenticationForm()
         context = {'form':form}
--- website/forms.py
+++ website/forms.py
@@ -1,8 +1,6 @@
 from django import forms
 from website.models import Contact, Newsletter
 from captcha.fields import CaptchaField
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 
 class ContactForm(forms.ModelForm):
     captcha = CaptchaField()
@@ -15,9 +13,3 @@ class NewsletterForm(forms.ModelForm):
     class Meta():
         model = Newsletter
         fields = '__all__'
-
-class RegisterForm(UserCreationForm):
-    email = forms.EmailField()
-    class Meta():
-        model = User
-        fields = ('username', 'email','password1', 'password2')
